c2cbadfe6f28499667e5d862530f1434ac4d6f5e
Ida Ligaard <cph-il95@cphbusiness.dk>
2023-09-07 12:06:26 +0200

    tilføjet env og gitignore

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+require ".env";
+
+
+$password = getenv ("PASSWORD");
+
+$servername = "localhost";
+$username = "root";
+$password = getenv("PASSWORD");
+
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=mul", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo " Connected successfully";
+} catch(PDOException $e){
+    echo " Connection failed: " . $e->getMessage();
+}
+
+$statement = $conn->query("select * from pipper");
+$result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+echo json_encode($result);
 ?>
