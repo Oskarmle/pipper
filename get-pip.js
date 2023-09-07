5d67f766d83c3e93bd478359bf05662382976e96
Ida Ligaard <cph-il95@cphbusiness.dk>
2023-09-07 13:58:29 +0200

    fået hjælp af christian

--- get-pip.js
+++ get-pip.js
@@ -1,2 +1,12 @@
-const localhost = "127.0.0.1:8000"
+const URL = "http://127.0.0.1:8000";
 
+export async function fetchPips() { //vi har skrevet async for at fortaelle at det er en asynkron funktion
+    const response = await fetch(URL, { //da det er et object skal den have {}
+        
+    });
+    const body = await response.json(); 
+    //await er aksynkron (hente noget et andet sted, samtidig med det der foregaar her)
+    console.log(body);
+    
+    return body;
+    }
---
+++ script.js
@@ -0,0 +1,20 @@
+import { fetchPips } from "./get-pip.js";
+console.log("hej");
+async function load() { //eftersom fetchflowers er asynkron, saa skal load ogsaa vaere
+    const body = await fetchPips(); //body er alle vores blomster
+ 
+    body.forEach((pip)=> { //der staar body fordi det er vores nye array
+    //    createPip(pip);
+
+//    document.getElementById('body').appendChild
+    
+    const para = document.createElement("p");
+    para.innerText = pip.text_area;
+    document.getElementById('body').appendChild(para);
+    
+    
+    //console.log(pip);
+    });
+ }
+ 
+ load();
