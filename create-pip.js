8630276936fac1b175625b3bdcbb9050180a0caa
Oskar Eriksen <omle.eriksen@gmail.com>
2023-09-07 12:52:26 +0200

    arbejdet videre med loop

--- create-pip.js
+++ create-pip.js
@@ -8,15 +8,30 @@ function createPip(x) {
   //   clone template, så vi ikke ***** noget up
   const clone = document.importNode(template.content, true);
 
-  // Vi skal udfylde avatar
+  // Vi skal udfylde avatar id=avatar
+  const avatarClone = clone.getElementById("avatar");
+  avatarClone.src = x.avatarURL;
+  avatarClone.id = "avatar-" + x.id;
 
-  // Vi skal udfylkde username
+  // Vi skal udfylde username id=bruger
+  const usernameClone = clone.getElementById("bruger");
+  usernameClone.id = "username" + x.id;
+  usernameClone.innerText = x.username;
 
-  // Vi skal udfylde textarea
+  // Vi skal udfylde textarea id=pip-text
+  const piptextClone = clone.getElementById("pip-text");
+  piptextClone.id = "piptext" + x.id;
+  piptextClone.innerText = x.text_area;
 
-  // Vi skal måske udfylde billede
+  // Vi skal måske udfylde billede id=img
+  const img = clone.getElementById("img");
+  img.src = x.imgURL;
+  img.id = "image-" + x.id;
 
-  // Vi skal udfylde dato og tid
+  // Vi skal udfylde dato og tid id=datetime
+  const datetimeClone = clone.getElementById("datetime");
+  datetimeClone.id = "datetime" + x.id;
+  datetimeClone.innerText = x.date;
 
   //   Vi sætter den udfyldte clone ind i vores Main på vores HTML
   mainContent.appendChild(clone);
