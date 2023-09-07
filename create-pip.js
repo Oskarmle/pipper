33ff7bfd7318f09da6b967b5c9181674ec5280a8
Oskar Eriksen <omle.eriksen@gmail.com>
2023-09-07 12:16:21 +0200

    begynde at oprette funktion i CreatePip.js

--- create-pip.js
+++ create-pip.js
@@ -1,2 +1,10 @@
 // Variable til main hvor alle pips er placeret
 const mainContent = document.getElementById("main");
+
+function createPip(x) {
+  // Vi skal finde templame med ID fra HTML dokument
+  const template = document.getElementById("PipTemplate");
+
+  //   clone template, så vi ikke ***** noget up
+  const clone = document.importNode(template.contentEditable, true);
+}
