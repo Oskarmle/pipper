3a5c6d5c1c29c7a0e9589748e1c8aff5c82c2746
Oskar Eriksen <omle.eriksen@gmail.com>
2023-09-07 12:25:04 +0200

    arbejdet videre med funktionen der skal lave pips

--- create-pip.js
+++ create-pip.js
@@ -6,5 +6,18 @@ function createPip(x) {
   const template = document.getElementById("PipTemplate");
 
   //   clone template, så vi ikke ***** noget up
-  const clone = document.importNode(template.contentEditable, true);
+  const clone = document.importNode(template.content, true);
+
+  // Vi skal udfylde avatar
+
+  // Vi skal udfylkde username
+
+  // Vi skal udfylde textarea
+
+  // Vi skal måske udfylde billede
+
+  // Vi skal udfylde dato og tid
+
+  //   Vi sætter den udfyldte clone ind i vores Main på vores HTML
+  mainContent.appendChild(clone);
 }
