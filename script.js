2b36871a39ba3e0209429ded4b76bfd421735898
Ida Ligaard <cph-il95@cphbusiness.dk>
2023-09-13 12:33:23 +0200

    der er ikke ændret noget

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { fetchPips } from "./get-pip.js";
 console.log("hej");
-async function load() { //eftersom fetchflowers er asynkron, saa skal load ogsaa vaere
-    const body = await fetchPips(); //body er alle vores blomster
+async function load() { //eftersom fetchPips er asynkron, saa skal load ogsaa vaere
+    const body = await fetchPips(); //body er alle vores pips
  
     body.forEach((pip)=> { //der staar body fordi det er vores nye array
     //    createPip(pip);
