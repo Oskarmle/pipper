0b179962a45454c30f3cf02c70410fca734a1898
Oskar Eriksen <omle.eriksen@gmail.com>
2023-09-14 09:43:35 +0200

    oprettet ny js fil og lavet små ændringer

---
+++ image-helpers.js
@@ -0,0 +1,13 @@
+export const imageToBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (e) => reject(e);
+  });
+
+export function base64ToImage(base64) {
+  var image = new Image();
+  image.src = base64;
+  return image;
+}
--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+// Vi importere den funktion som henter pips fra
 import { fetchPips } from "./get-pip.js";
+
 console.log("hej");
-async function load() { //eftersom fetchPips er asynkron, saa skal load ogsaa vaere
-    const body = await fetchPips(); //body er alle vores pips
- 
-    body.forEach((pip)=> { //der staar body fordi det er vores nye array
+async function load() {
+  //eftersom fetchPips er asynkron, saa skal load ogsaa vaere
+  const body = await fetchPips(); //body er alle vores pips
+
+  body.forEach((pip) => {
+    //der staar body fordi det er vores nye array
     //    createPip(pip);
 
-//    document.getElementById('body').appendChild
-    
+    //    document.getElementById('body').appendChild
+
     const para = document.createElement("p");
     para.innerText = pip.text_area;
-    document.getElementById('body').appendChild(para);
-    
-    
+    document.getElementById("body").appendChild(para);
+
     //console.log(pip);
-    });
- }
- 
- load();
+  });
+}
+
+load();
