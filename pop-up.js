297a11f57322827118119981cad6074a73b5a2f2
Ida Ligaard <cph-il95@cphbusiness.dk>
2023-09-14 12:41:06 +0200

    jeg har problemmer

--- pop-up.js
+++ pop-up.js
@@ -1,9 +1,25 @@
-// function myFunction() {
-//     var popup = document.getElementById("myPopup");
-//     popup.classList.toggle("show");
-//   }
 
-function pipclickfunction() {
-  let popup = document.getElementById("popup-container");
-  popup.classList.toggle("show");
+// function pipclickfunction() {
+//   let popup = document.getElementById("popup-container");
+//   popup.classList.toggle("show");
+// }
+
+
+let elem = document.getElementsByClassName('send:after');
+
+for(let i = 0; i < elem.length; i++) {
+ elem[i].addEventListener("click", function(){ 
+    
+            document.querySelector(".popup").style.display = "block";
+       
+
+     
+ }, false);
 }
+    
+
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
