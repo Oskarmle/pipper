c94ddddcc8ae8b342534022bb695c19dc5a65dbf
Oskar Eriksen <omle.eriksen@gmail.com>
2023-09-14 11:53:33 +0200

    en masse ændringer i forhold til base64

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ import { fetchPips } from "./get-pip.js";
 
 import { createPip } from "./create-pip.js";
 
+import { imageToBase64 } from "./image-helpers.js";
+
+const form = document.getElementById("create-pip-form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const data = new FormData(form);
+
+  const username = data.get("username");
+  const text = data.get("add-text");
+  const image = data.get("image");
+  const base64 = await imageToBase64(image);
+
+  // Vi opretter et objekt
+  const pipObject = {
+    username: username,
+    text_area: text,
+    img_base64: base64,
+  };
+});
+
 async function load() {
   //eftersom fetchPips er asynkron, saa skal load ogsaa vaere
   const body = await fetchPips(); //body er alle vores pips
