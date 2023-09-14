f81f543a1870e5c045297c69ee9edc84ebeb906b
Oskar Eriksen <omle.eriksen@gmail.com>
2023-09-14 10:27:18 +0200

    fået template til at virke

--- create-pip.js
+++ create-pip.js
@@ -1,37 +1,37 @@
 // Variable til main hvor alle pips er placeret
 const mainContent = document.getElementById("main");
 
-function createPip(x) {
+export function createPip(pip) {
   // Vi skal finde templame med ID fra HTML dokument
-  const template = document.getElementById("PipTemplate");
+  const template = document.getElementById("pipTemplate");
 
   //   clone template, så vi ikke ***** noget up
   const clone = document.importNode(template.content, true);
 
   // Vi skal udfylde avatar id=avatar
-  const avatarClone = clone.getElementById("avatar");
-  avatarClone.src = x.avatarURL;
-  avatarClone.id = "avatar-" + x.id;
+  // const avatarClone = clone.getElementById("avatar");
+  // avatarClone.src = x.avatarURL;
+  // avatarClone.id = "avatar-" + pip.id;
 
   // Vi skal udfylde username id=bruger
   const usernameClone = clone.getElementById("bruger");
-  usernameClone.id = "username" + x.id;
-  usernameClone.innerText = x.username;
+  usernameClone.id = "username" + pip.id;
+  usernameClone.innerText = pip.username;
 
   // Vi skal udfylde textarea id=pip-text
   const piptextClone = clone.getElementById("pip-text");
-  piptextClone.id = "piptext" + x.id;
-  piptextClone.innerText = x.text_area;
+  piptextClone.id = "piptext" + pip.id;
+  piptextClone.innerText = pip.text_area;
 
   // Vi skal måske udfylde billede id=img
   const img = clone.getElementById("img");
-  img.src = x.imgURL;
-  img.id = "image-" + x.id;
+  img.src = pip.imgURL;
+  img.id = "image-" + pip.id;
 
   // Vi skal udfylde dato og tid id=datetime
   const datetimeClone = clone.getElementById("datetime");
-  datetimeClone.id = "datetime" + x.id;
-  datetimeClone.innerText = x.date;
+  datetimeClone.id = "datetime" + pip.id;
+  datetimeClone.innerText = pip.date;
 
   //   Vi sætter den udfyldte clone ind i vores Main på vores HTML
   mainContent.appendChild(clone);
--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 // Vi importere den funktion som henter pips fra
 import { fetchPips } from "./get-pip.js";
 
-console.log("hej");
+import { createPip } from "./create-pip.js";
+
 async function load() {
   //eftersom fetchPips er asynkron, saa skal load ogsaa vaere
   const body = await fetchPips(); //body er alle vores pips
 
   body.forEach((pip) => {
     //der staar body fordi det er vores nye array
-    //    createPip(pip);
-
-    //    document.getElementById('body').appendChild
-
-    const para = document.createElement("p");
-    para.innerText = pip.text_area;
-    document.getElementById("body").appendChild(para);
-
-    //console.log(pip);
+    createPip(pip);
   });
 }
 
