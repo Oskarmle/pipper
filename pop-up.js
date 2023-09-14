d7c4e7fd31f6af8b3288cdc3c0bdb199dd5edf26
Emilie Midtgaard <cph-em134@cphbusiness.dk>
2023-09-14 14:17:41 +0200

    popup

--- pop-up.js
+++ pop-up.js
@@ -1,25 +1,58 @@
 
+
+document.getElementById("pip").addEventListener("click", function(){
+    console.log("hej fra function")
+    document.querySelector(".popup_container").style.display = "flex";
+})
+
+
+
+document.querySelector(".post-knap").addEventListener("click", function(){
+    console.log("nej")
+    document.querySelector(".popup_container").style.display = "none";
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function pipclickfunction() {
 //   let popup = document.getElementById("popup-container");
 //   popup.classList.toggle("show");
 // }
 
 
-let elem = document.getElementsByClassName('send:after');
+// let elem = document.getElementsByClassName('send:after');
 
-for(let i = 0; i < elem.length; i++) {
- elem[i].addEventListener("click", function(){ 
+// for(let i = 0; i < elem.length; i++) {
+//  elem[i].addEventListener("click", function(){ 
     
-            document.querySelector(".popup").style.display = "block";
+//             document.querySelector(".popup").style.display = "block";
        
 
      
- }, false);
-}
+//  }, false);
+// }
     
 
 
 
-document.querySelector("#close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
-});
+// document.querySelector("#close").addEventListener("click", function(){
+//     document.querySelector(".popup").style.display = "none";
+// });
+
+
