d25adc643ae8d51be5847f687e2129e90f4da7f0
Ida Ligaard <cph-il95@cphbusiness.dk>
2023-09-14 23:17:45 +0200

    255 characters virker nu. juhuuu

--- count.js
+++ count.js
@@ -1,13 +1,14 @@
 
-const new_text = document.getElementById("new-text");
+// const new_text = document.getElementById("new-text");
 
-let myCount = document.getElementById("new-text");
-myCount.addEventListener("input", () => {
-    let count = (myCount.value).length;
-    document.getElementById("countresult").textContent = `Total characters: ${count}`;
+// let myCount = document.getElementById("new-text");
+// myCount.addEventListener("input", () => {
+//     let count = (myCount.value).length;
+//     document.getElementById("countresult").textContent = `Total characters: ${count}`;
+// });
 
-
+document.getElementById('new-text').addEventListener('keyup', e=> {
+    document.getElementById("countresult").textContent = 255 - e.target.value.length;
+    e.target.value = e.target.value.substring(0, 255);
 });
-
-
 
