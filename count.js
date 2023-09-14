e82ca2d45b76b8a59c1dd65268407f731d8679f6
Ida Ligaard <cph-il95@cphbusiness.dk>
2023-09-14 15:49:27 +0200

    tæller op:)

--- count.js
+++ count.js
@@ -1,5 +1,13 @@
+
+const new_text = document.getElementById("new-text");
+
 let myCount = document.getElementById("new-text");
 myCount.addEventListener("input", () => {
     let count = (myCount.value).length;
     document.getElementById("countresult").textContent = `Total characters: ${count}`;
+
+
 });
+
+
+
